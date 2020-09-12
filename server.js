f3b510cb83fc6a69de03d41b0efdc0152d2e4689
Federico <cuervog992@gmail.com>
2020-09-12 16:59:05 -0300

    creacion servidor

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-q;
+/*const express = require("express");
+const csv = require("csv-parser");
+const fs = require("fs");
+const Sequelize = require("sequelize");
+const express = require("express");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+const sequelize = new Sequelize("mysql://root@localhost/delilahresto");
+
+app.use(bodyParser.json());
+
+app.listen(3000, () => {
+  console.log("Servidor iniciando");
+});*/
